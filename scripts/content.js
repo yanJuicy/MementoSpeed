f84cd8c99b1eaa08d9f1362b0a8e12b222c3dceb
yanJuicy <hanjub3@gmail.com>
2026-07-22 18:33:07 +0900

    video 페이지에서만 스크립트 실행 & 새로운 페이지 변경 확인

--- scripts/content.js
+++ scripts/content.js
@@ -1,25 +1,37 @@
-// <video> 태그가 존재하면 속도 조절
-const video = document.querySelector("video");
-if (video) {
-  video.playbackRate = 2;
-}
-
 // URL에서 videoId를 가져오기
 function getCurrentVideoId() {
   return new URL(location.href).searchParams.get("v");
 }
 
+// video 시청 페이지인지 확인
+function checkVideoPage() {
+  return location.pathname === "/watch";
+}
+
 let currentVideoId = getCurrentVideoId();
 
-// video 변경 감지
-const targetNode = video;
-const config = { attributes: true };
+const targetNode = document.body;
+const config = { subtree: true, childList: true };
 const callback = (mutationsList, observer) => {
+  // 비디오 페이지가 아니면 종료
+  if (!checkVideoPage()) {
+    return;
+  }
+
+  // <video> 태그가 존재하면 속도 조절
+  const video = document.querySelector("video");
+  if (!video) {
+    return;
+  }
+
   for (const mutation of mutationsList) {
-    if (mutation.type === "attributes" && mutation.attributeName === "src") {
+    if (mutation.type === "childList") {
       const newVideoId = getCurrentVideoId();
       if (currentVideoId !== newVideoId) {
         console.log("영상 변경!");
+        console.log(video);
+        console.log(video.readyState);
+        console.log(video.playbackRate);
         currentVideoId = newVideoId;
         video.playbackRate = 2;
       }
